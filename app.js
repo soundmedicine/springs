d9bda16e136390f727e4a9ee61fb24e93ef1c168
Roberto Santos <robot-o@hotmail.com>
2019-01-15 13:48:08 -0800

    install nodemon and add springs route

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var createError = require('http-errors');
+const express = require('express');
+const path = require('path');
 
-var app = express();
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const springs = require('./api/springs');
+
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use('/api/v1/springs', springs);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
