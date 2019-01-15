aa5ae2e9469fae2ca700f2798f26971ef3f6906b
Roberto Santos <robot-o@hotmail.com>
2019-01-15 14:28:20 -0800

    transfer data object to springs.js file

---
+++ springs.js
@@ -0,0 +1,31 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Glenwood Hot Springs',
+        description: 'Largest hot springs establishment in Colorado, right off I-70.',
+        location: 'Glenwood Springs, CO',
+        access_rating: 4,
+        url: 'https://www.hotspringspool.com/'
+      },{
+        id: 2,
+        name: 'Radium Hot Springs',
+        description: 'Undeveloped hot springs off the Colorado River. Multiple trailheads off of Trough Road, south of Kremmling, north of Silverthorne.',
+        location: 'Radium, CO',
+        access_rating: 0,
+        url: 'https://www.uncovercolorado.com/hot-springs/radium-hot-springs/'
+      }, {
+        id: 3,
+        name: 'Iron Mountain Hot Springs',
+        description: 'Newest hot springs establishment in Glenwood Springs.',
+        location: 'Glenwood Springs, CO',
+        access_rating: 1,
+        url: 'https://www.ironmountainhotsprings.com/'
+      }, {
+        id: 4,
+        name: 'Penny Hot Springs',
+        description: 'Undeveloped hot springs on Avalanche Creek, right off Highway 133.',
+        location: 'Carbondale, CO',
+        access_rating: 0,
+        url: 'https://www.uncovercolorado.com/hot-springs/penny-hot-springs/'
+      }
+]
--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,28 @@
+const request = require('supertest');
+const expect = require('chai').expect;
 const knex = require('../db/knex');
 
+
+const app = require('../app');
+
 describe('CRUD Springs', () => {
-    before(() => {
-        //run migrations
+    before((done) => {
         knex.migrate.latest()
             .then(() => {
-             //run seeds
             return knex.seed.run();
-        })
+        }).then(() => done());
+    });
+
+    it('Lists all records ...', (done) => {
+        request(app)
+            .get('/api/v1/springs')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).to.be.a('array');
+                console.log(response.body);
+                done();
+            })
     })
-})
+});
