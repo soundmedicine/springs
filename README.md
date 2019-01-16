98f63ae77d7f63bc92e99f23643d7e85330b9cb8
Roberto Santos <robot-o@hotmail.com>
2019-01-15 16:39:21 -0800

    Add production DB connection

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## ERD
 
 ![](https://www.lucidchart.com/publicSegments/view/f1936259-7d4c-4475-9906-c1fef33cd1fb/image.png)
+
+https://crud-springs-api.herokuapp.com/
--- knexfile.js
+++ knexfile.js
@@ -9,4 +9,9 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/test-springs'
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
 };
+
